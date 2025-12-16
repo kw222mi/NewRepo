8dcf48afc75fcab55690d4aecfa11322d4f46a83
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-12-16 15:50:53 +0100

    add in controller

--- Test_projekt/Controllers/WeatherForecastController.cs
+++ Test_projekt/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Test_projekt.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "SuperHot" , "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
